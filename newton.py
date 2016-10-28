8c51a13e3b0dae8a62619188fd086af617dfe24b
Heather Prince <heather.prince1@gmail.com>
2016-10-28 19:37:38 -0400

    Added analytic Jacobian functionality to newton and added tests to testNewton

--- newton.py
+++ newton.py
@@ -7,15 +7,17 @@ import numpy as N
 import functions as F
 
 class Newton(object):
-    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6):
+    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6, Df=None):
         """Return a new object to find roots of f(x) = 0 using Newton's method.
         tol:     tolerance for iteration (iterate until |f(x)| < tol)
         maxiter: maximum number of iterations to perform
-        dx:      step size for computing approximate Jacobian"""
+        dx:      step size for computing approximate Jacobian
+        Df:      A function for the analytic Jacobian """
         self._f = f
         self._tol = tol
         self._maxiter = maxiter
         self._dx = dx
+        self._Df=Df
 
     def solve(self, x0):
         """Return a root of f(x) = 0, using Newton's method, starting from
@@ -26,7 +28,7 @@ class Newton(object):
             if N.linalg.norm(fx) < self._tol:
                 return x
             x = self.step(x, fx)
-        if self._f(x) > self._tol:
+        if N.linalg.norm(self._f(x)) > self._tol:
             raise RuntimeError ("The root was not found within the maximum number of %d iterations" % ( self._maxiter ))
         return x
 
@@ -35,6 +37,9 @@ class Newton(object):
         If the argument fx is provided, assumes fx = f(x)"""
         if fx is None:
             fx = self._f(x)
-        Df_x = F.ApproximateJacobian(self._f, x, self._dx)
+        if self._Df==None:
+            Df_x = F.ApproximateJacobian(self._f, x, self._dx)
+        else:
+            Df_x=self._Df(x)
         h = N.linalg.solve(N.matrix(Df_x), N.matrix(fx))
         return x - h
--- testNewton.py
+++ testNewton.py
@@ -4,6 +4,9 @@ import newton
 import unittest
 import numpy as N
 import functions as F
+#from mock import patch - I was going to use this to test that analytic derivative was called but then realised that mock
+#isn't necessarily part of a standard python install so I took it out
+
 
 class TestNewton(unittest.TestCase):
     def testLinear(self):
@@ -12,11 +15,82 @@ class TestNewton(unittest.TestCase):
         x = solver.solve(2.0)
         self.assertEqual(x, -2.0)
 
+    def testQuadratic(self):
+        p=F.Polynomial([1,-1,-6])    #x^2-x+6 has roots 3,-2
+        solver = newton.Newton(p, tol=1.e-15, maxiter=20)
+        x1=solver.solve(3.5)
+        x2=solver.solve(-1.5)
+        self.assertEqual(x1, 3.0)
+        self.assertEqual(x2, -2.0)
+
+    def testQuadratic2D(self):
+        q1=F.BivariateQuadratic2D([2,0,0,0,0,0],[0,2,0,0,0,0]) #x^2+y^2
+        guess=N.matrix("0.1;-0.1")
+        solver = newton.Newton(q1, tol=1.e-13, maxiter=50)
+        x=solver.solve(guess)
+        self.assertEqual(x.shape, (2,1))
+        N.testing.assert_array_almost_equal(x, N.matrix("0;0"))
+
+    def testCubic(self):
+        p = F.Polynomial([1,0,1,0]) #root is x=0
+        solver = newton.Newton(p, tol=1.e-15, maxiter=50)
+        x1=solver.solve(0.1)
+        x2=solver.solve(-0.1)
+        self.assertAlmostEqual(x1, 0.)
+        self.assertAlmostEqual(x2, 0.)
+
     def testMaxIterException(self):
         p = F.Polynomial([1,0,0,0])
         solver = newton.Newton(p, tol=1.e-15, maxiter=2)
         guess=5.0   #would take more than 2 iterations to reach 0 from here
         self.assertRaises(RuntimeError, solver.solve, guess)
+
+    def testLinearAnalyticJac(self):
+        f = lambda x : 3.0 * x + 6.0
+        df= lambda x: N.matrix("3.0")
+        solver = newton.Newton(f, tol=1.e-15, maxiter=2, Df=df)
+        x = solver.solve(2.0)
+        self.assertEqual(x, -2.0)
+
+    def testQuadraticAnalyticJac(self):
+        p=F.Polynomial([1,-1,-6])    #x^2-x+6 has roots 3,-2
+        solver = newton.Newton(p, tol=1.e-15, maxiter=20, Df=p.Jacobian)
+        x1=solver.solve(3.5)
+        x2=solver.solve(-1.5)
+        self.assertEqual(x1, 3.0)
+        self.assertEqual(x2, -2.0)
+
+    def testQuadratic2DAnalyticJac(self):
+        q=F.BivariateQuadratic2D([2,0,0,0,0,0],[0,2,0,0,0,0]) #x^2+y^2
+        guess=N.matrix("0.1;-0.1")
+        solver = newton.Newton(q, tol=1.e-13, maxiter=50, Df=q.Jacobian)
+        x=solver.solve(guess)
+        self.assertEqual(x.shape, (2,1))
+        N.testing.assert_array_almost_equal(x, N.matrix("0;0"))
+
+    def testCubicAnalyticJac(self):
+        p = F.Polynomial([1,0,1,0]) #root is x=0
+        solver = newton.Newton(p, tol=1.e-15, maxiter=50, Df=p.Jacobian)
+        x1=solver.solve(0.1)
+        x2=solver.solve(-0.1)
+        self.assertAlmostEqual(x1, 0.)
+        self.assertAlmostEqual(x2, 0.)
+
+    def testSinusoidalAnalyticJac(self):
+        s=F.Sinusoid(2., 5., 0.)
+        solver = newton.Newton(s, tol=1.e-15, maxiter=50, Df=s.Jacobian)
+        x1=solver.solve(0.1)
+        x2=solver.solve(-0.1)
+        self.assertAlmostEqual(x1, 0.)
+        self.assertAlmostEqual(x2, 0.)
+
+    def testStep(self):
+        f=lambda x: 1/2.*x**2
+        solver = newton.Newton(f, tol=1.e-15, maxiter=20, Df=lambda x:1.*x)
+        x_new=solver.step(10.)
+        self.assertAlmostEqual(x_new, 5.)
+
+
 
 if __name__ == "__main__":
     unittest.main()
