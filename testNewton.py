16fa328c9c87c8bccf90619a96cced8d108d58b9
Heather Prince <heather.prince1@gmail.com>
2016-10-27 21:19:59 -0400

    Added test for max iterations exception in testNewton

--- testNewton.py
+++ testNewton.py
@@ -3,6 +3,7 @@
 import newton
 import unittest
 import numpy as N
+import functions as F
 
 class TestNewton(unittest.TestCase):
     def testLinear(self):
@@ -10,6 +11,12 @@ class TestNewton(unittest.TestCase):
         solver = newton.Newton(f, tol=1.e-15, maxiter=2)
         x = solver.solve(2.0)
         self.assertEqual(x, -2.0)
+
+    def testMaxIterException(self):
+        p = F.Polynomial([1,0,0,0])
+        solver = newton.Newton(p, tol=1.e-15, maxiter=2)
+        guess=5.0   #would take more than 2 iterations to reach 0 from here
+        self.assertRaises(RuntimeError, solver.solve, guess)
 
 if __name__ == "__main__":
     unittest.main()
